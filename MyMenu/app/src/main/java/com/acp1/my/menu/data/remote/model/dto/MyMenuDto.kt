04264907311dce59b477b6670440f984f9731cab
luber <tomas.lubertino@gmail.com>
2019-11-06 13:05:24 -0300

    se agrego el host y se hicieron algunos fixes de la conexion

--- MyMenu/app/src/main/java/com/acp1/my/menu/data/remote/model/dto/MyMenuDto.kt
+++ MyMenu/app/src/main/java/com/acp1/my/menu/data/remote/model/dto/MyMenuDto.kt
@@ -1,6 +1,5 @@
 package com.acp1.my.menu.data.remote.model.dto
 
-import com.acp1.my.menu.data.remote.model.ApiResponse
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
@@ -18,14 +17,13 @@ data class CategoryDto(
 
 @JsonClass(generateAdapter = true)
 data class DishesDto(
-    @Json(name = "status") override val status: String,
     @Json(name = "id") val id: String,
     @Json(name = "nombre") val name: String,
     @Json(name = "imagen") val picture: String,
     @Json(name = "precio") val price: String,
     @Json(name = "descripcion") val description: String,
     @Json(name = "filtros") val filters: List<FilterDto>?
-) : ApiResponse()
+)
 
 @JsonClass(generateAdapter = true)
 data class DishDto(
@@ -37,9 +35,8 @@ data class DishDto(
 
 @JsonClass(generateAdapter = true)
 data class FiltersDto(
-    @Json(name = "status") override val status: String,
     @Json(name = "filtros") val filters: List<FilterDto>?
-) : ApiResponse()
+)
 
 @JsonClass(generateAdapter = true)
 data class FilterDto(
@@ -49,9 +46,8 @@ data class FilterDto(
 
 @JsonClass(generateAdapter = true)
 data class PromotionsDto(
-    @Json(name = "status") override val status: String,
     @Json(name = "promociones") val promotions: List<PromotionDto>
-) : ApiResponse()
+)
 
 @JsonClass(generateAdapter = true)
 data class PromotionDto(
@@ -62,9 +58,8 @@ data class PromotionDto(
 
 @JsonClass(generateAdapter = true)
 data class PaymentsDto(
-    @Json(name = "status") override val status: String,
     @Json(name = "medios-de-pago") val payments: List<PaymentDto>
-) : ApiResponse()
+)
 
 @JsonClass(generateAdapter = true)
 data class PaymentDto(
@@ -75,14 +70,13 @@ data class PaymentDto(
 
 @JsonClass(generateAdapter = true)
 data class TodayMenuDto(
-    @Json(name = "status") override val status: String,
     @Json(name = "entrada") val starter: OptionDto,
     @Json(name = "plato-principal") val main: OptionDto,
     @Json(name = "precio") val price: String,
     @Json(name = "postre") val dessert: OptionDto,
     @Json(name = "cafe") val hasCoffee: Boolean,
     @Json(name = "opciones") val options: String
-) : ApiResponse()
+)
 
 @JsonClass(generateAdapter = true)
 data class OptionDto(
